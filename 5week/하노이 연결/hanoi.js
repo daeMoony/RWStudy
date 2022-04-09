569421c9cea83d1117f34fd4a0bcdc8d211c1365
daeMoony <shiert12@gmail.com>
2022-04-09 14:45:53 +0900

    하노이 수정본

---
+++ 5week/하노이 연결/hanoi.js
@@ -0,0 +1,9 @@
+function _RedBox() {
+    document.write("<p>🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥</p><p>🟨🟨🟨🟨🟨🟨🟨🟨🟨</p><p>🟩🟩🟩🟩🟩🟩🟩</p><p>🟦🟦🟦🟦🟦</p><p>🟪🟪🟪</p><p>⬛</p>")
+}
+function _GreenBox() {
+    document.write("<p>second</p>")
+}
+function _show() {
+    document.getElementById("Hano").style.display="block";
+}
