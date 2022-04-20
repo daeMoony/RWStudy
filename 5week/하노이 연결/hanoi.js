89c4b04b8a358ce7ce8f54109e676df44a35a614
daeMoony <101255209+daeMoony@users.noreply.github.com>
2022-04-20 15:53:01 +0900

    Update hanoi.js

--- 5week/하노이 연결/hanoi.js
+++ 5week/하노이 연결/hanoi.js
@@ -1,9 +1,35 @@
-function _RedBox() {
-    document.write("<p>🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥</p><p>🟨🟨🟨🟨🟨🟨🟨🟨🟨</p><p>🟩🟩🟩🟩🟩🟩🟩</p><p>🟦🟦🟦🟦🟦</p><p>🟪🟪🟪</p><p>⬛</p>")
+var _red = "<br>🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥";
+var _yellow = "<br>🟨🟨🟨🟨🟨🟨🟨🟨🟨";
+var _green = "<br>🟩🟩🟩🟩🟩🟩🟩";
+var _blue = "<br>🟦🟦🟦🟦🟦";
+var _puple = "<br>🟪🟪🟪";
+var _black = "<br>⬛";
+var hand_red = 0;
+var hand_yellow = 0;
+var hand_green = 0;
+var hand_blue = 0;
+var hand_purple = 0;
+var hand_black = 0;
+var _1arr = ["<br>🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥","<br>🟨🟨🟨🟨🟨🟨🟨🟨🟨","<br>🟩🟩🟩🟩🟩🟩🟩","<br>🟦🟦🟦🟦🟦","<br>🟪🟪🟪","<br>⬛"]
+var _2arr = []
+var _3arr = []
+
+var a = document.getElementsByClassName("A")
+
+function _click1() {
+
 }
-function _GreenBox() {
-    document.write("<p>second</p>")
+function _click2() {
+    
 }
-function _show() {
-    document.getElementById("Hano").style.display="block";
+function _click3() {
+    
+}
+function _startBtn(){
+   render()
+}
+
+function render(){
+    a[0].innerHTML = _1arr;
+    a[0].innerHTML = "";
 }
